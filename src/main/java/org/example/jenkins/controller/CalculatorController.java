e5b89e911f1e4536f6d4895ad26a43fdb914f181
PBEM22 <dlacofbs1229@gmail.com>
2024-11-06 00:18:01 +0900

    jenkine commit

--- src/main/java/org/example/jenkins/controller/CalculatorController.java
+++ src/main/java/org/example/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String health() {
-        return "Alive";
+        return "Jenkins Alive!!";
     }
 
     @GetMapping("/plus")
